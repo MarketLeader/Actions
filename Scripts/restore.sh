beb3d7d6bf696cf0399f1488d05f12ca0d736942
eq19 <admin@eq19.com>
2022-11-08 18:22:39 +0700

    Update restore.sh

--- Scripts/restore.sh
+++ Scripts/restore.sh
@@ -3,7 +3,7 @@
 cd /maps/default/feed
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
-git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; chown -R $(whoami) .git/config; git config --unset http.https://github.com/.extraheader; git config --unset-all http.https://github.com/.extraheader; git config --global --unset http.https://github.com/.extraheader; git config --system --unset http.https://github.com/.extraheader;git clean -dfx'
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; chown -R $(whoami) .git; git config --unset http.https://github.com/.extraheader; git config --unset-all http.https://github.com/.extraheader; git config --global --unset http.https://github.com/.extraheader; git config --system --unset http.https://github.com/.extraheader;git clean -dfx'
 
 # Restore the modification time (mtime) of all files in work tree, based on the
 # date of the most recent commit that modified the file.
@@ -22,7 +22,7 @@ mv _plugins ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_plugins _plugins
 mv _includes ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_includes _includes
 
 cd /maps/default/datasets
-mv /text ${JEKYLL_SRC}/_data && ln -s ${JEKYLL_SRC}/_data data
+mv text ${JEKYLL_SRC}/_data && ln -s ${JEKYLL_SRC}/_data data
 
 cd ${JEKYLL_SRC}/_data/_base
 mv _maps ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_maps _maps

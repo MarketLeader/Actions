558108e540bd44c7c1861a874372bf9885150eb6
eq19 <admin@eq19.com>
2022-12-04 13:05:46 +0700

    submodule update

--- Scripts/init_environment.sh
+++ Scripts/init_environment.sh
@@ -22,6 +22,10 @@ git config --unset http.https://github.com/.extraheader
 git config --unset-all http.https://github.com/.extraheader
 git config --global --unset http.https://github.com/.extraheader
 git config --system --unset http.https://github.com/.extraheader
+git submodule update --init --recursive
+git submodule foreach --recursive git fetch
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); chown -R $(whoami) .git; chmod -R +x .git; git config --unset http.https://github.com/.extraheader; git config --unset-all http.https://github.com/.extraheader; git config --global --unset http.https://github.com/.extraheader; git config --system --unset http.https://github.com/.extraheader;git submodule update --recursive; git clean -dfx'
+
 cat .git/config
 
 # environtment
